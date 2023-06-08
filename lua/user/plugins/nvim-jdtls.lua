d51ccdfd4962258011392cab12f655f46c970787
cold <coldmind2@gmail.com>
2023-06-08 16:25:38 +0300

    Fixed bufferline config issue

--- lua/user/plugins/nvim-jdtls.lua
+++ lua/user/plugins/nvim-jdtls.lua
@@ -1,5 +1,11 @@
+local cmp_status_ok, jdtls = pcall(require, "jdtls")
+if not cmp_status_ok then
+	return
+end
+
+
 local config = {
     cmd = {'/home/cold/.local/share/nvim/mason/bin/jdtls'},
     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
 }
-require('jdtls').start_or_attach(config)
+jdtls.start_or_attach(config)
